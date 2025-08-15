22ca3b58ba193e4313b1c8396519b458a07195b1
jakep <jakeparperis@gmail.com>
2025-08-15 21:57:29 +0300

    Update sb_ids.py with all available competitions and their ids, split into two dicts based on gender

--- src/sb_ids.py
+++ src/sb_ids.py
@@ -1,22 +1,30 @@
-from statsbombpy import sb
-import pandas as pd
-
-pd.set_option('display.max_rows', 500)
-comps = sb.competitions()
-print(comps[['competition_id', 'competition_name', 'season_id', 'season_name']])
-
 # competition/season: id
 
-comp_ids = {
-    'euro': 55,
-    'copa': 223,
-    'afcon': 1267,
-    'ucl': 16,
-    'ligue1': 7,
-    'bundesliga': 9,
-    'laliga': 11,
-    'epl': 2,
-    'mls': 44
+mens_comp_ids = {
+    'english_premier_league': 2,
+    'french_ligue_1': 7,
+    'german_bundesliga': 9,
+    'spanish_la_liga': 11,
+    'italian_serie_a': 12,
+    'uefa_champions_league': 16,
+    'uefa_europa_league': 35,
+    'fifa_world_cup': 43,
+    'usa_major_league_soccer': 44,
+    'uefa_euro': 55,
+    'argentinian_liga_profesional': 81,
+    'spanish_copa_del_rey': 87,
+    'usa_north_american_league': 116,
+    'conmebol_copa_america': 223,
+    'indian_suger_league': 1238,
+    'african_cup_of_nations': 1267,
+    'fifa_u20_world_cup': 1470
+}
+
+womens_comp_ids = {
+    'english_wsl': 37,
+    'usa_nwsl': 49,
+    'womens_euro': 53,
+    'womens_world_cup': 72
 }
 
 season_ids_euro = {
@@ -39,7 +47,7 @@ season_ids_ucl = {
     '15/16': 27
 }
 
-season_ids_ligue1 = {
+season_ids_french_ligue_1 = {
     '22/23': 235,
     '21/22': 108,
     '15/16': 27
